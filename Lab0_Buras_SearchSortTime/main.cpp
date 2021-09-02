f344ac79e9ea3002680f5de033099dfca16bfe5c
Nicholas Buras <nicholas.buras@trojans.dsu.edu>
2021-09-01 19:44:45 -0700

    Initial Commit

---
+++ Lab0_Buras_SearchSortTime/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab0_Buras_SearchSortTime
+//
+//  Created by Nicholas Buras on 9/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
